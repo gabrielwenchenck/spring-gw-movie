a06d04ebf844397292f63b57a1d8efd505aa54bd
Gabriel <wenck13@gmail.com>
2023-09-30 17:01:32 -0300

    Navigation buttons

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,24 +1,32 @@
-import {ReactComponent as GithubIcon } from 'assets/img/gh-logo.svg'
-import {ReactComponent as Logo } from 'assets/img/logo.svg'
+import { ReactComponent as GithubIcon } from "assets/img/gh-logo.svg";
+import { ReactComponent as Logo } from "assets/img/logo.svg";
+import { Link } from "react-router-dom";
 
-function Navbar(){
-  return(<header>
-    <nav className="container">
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
         <div className="gwmovie-nav-content">
+          <Link to="/">
             <h1>
-                  <Logo/>
-                  GW Movie
+              <Logo />
+              GW Movie
             </h1>
-          
-            <a href="https://github.com/gabrielwenchenck" target="_blank" rel="noreferrer">
-                <div className="gwmovie-contact-container">
-                    <GithubIcon />
-                    <p className="gwmovie-contact-link">/gabrielwenchenck</p>
-                </div>
-            </a>
+          </Link>
+          <a
+            href="https://github.com/gabrielwenchenck"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="gwmovie-contact-container">
+              <GithubIcon />
+              <p className="gwmovie-contact-link">/gabrielwenchenck</p>
+            </div>
+          </a>
         </div>
-    </nav>
-</header>)
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
