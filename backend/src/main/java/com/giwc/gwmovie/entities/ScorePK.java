95e25e633b4c18f7ab823aea9a69f81ce8b30143
Gabriel <wenck13@gmail.com>
2023-10-03 14:34:08 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/giwc/gwmovie/entities/ScorePK.java
@@ -0,0 +1,39 @@
+package com.giwc.gwmovie.entities;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Embeddable
+public class ScorePK implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public ScorePK() {
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
